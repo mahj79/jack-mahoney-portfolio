f6fb6a8962fea62b27087f30cb01d9e57a57663c
mahj79 <mahonej79@gmail.com>
2023-04-09 19:41:13 -0500

    added some more about me info!

--- src/Components/AboutMe/AboutMeBox.tsx
+++ src/Components/AboutMe/AboutMeBox.tsx
@@ -12,20 +12,27 @@ export const AboutMeBox: React.FC = () => {
   return (
         <div className='AboutMeContainer'>
           <div className='AboutMeBox'>
-            <h1>About Me</h1>
+            <h1>About Me (Work in Progress)</h1>
             <img className="AboutMeImage" src="HawaiiVolcano.jpg" alt="Me in Hawaii!" />
             {visible && (
               <div className="AboutMeBoxText">
                 <p>
-                  Hello, my name is Jack Mahoney and I'm a web developer based in Chicago.
-                  I specialize in building responsive and user-friendly websites
-                  using the latest web technologies.
+                  Hello, my name is Jack Mahoney and I'm a developer based in Chicago.
+                  Since graduating college in 2018 with a degree in electrical engineering, I've spent my career
+                  working in the cybersecurity field! You can see the companies I've had the pleasure of working at up
+                  to this point below, give them a click to find out more about each role. 
+
+                  While my focus has been in the cybersecurity space, my real passion is simply sovling complex problems
+                  with tech, hence why I built this website! When I'm not working, you can usually find me trying to improve
+                  my programming skills or spending time with my friends, girlfriend, or Cleo my french bulldog.
+                  If you're interested in learning more about me, hop over to the next page to read about my programming journey.                   
+                  
                 </p>
-                <button>Download Resume</button>
+                <button >Download Resume</button>
               </div>
             )}
             <button onClick={toggleVisibility}>
-              {visible ? "Hide" : "Show More"}
+              {visible ? "Hide" : "Learn About Me!"}
             </button>
           </div>
 
