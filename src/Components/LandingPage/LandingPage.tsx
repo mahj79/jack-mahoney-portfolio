6ae08ac61ee03f9ffb804add56584293800da28f
mahj79 <mahonej79@gmail.com>
2023-05-01 20:00:15 -0500

    adding header for navigating website

--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -3,9 +3,17 @@ import React from 'react';
 export const LandingPage: React.FC = () => {
 
 return (
-    <div className="landingPageContainer">
-        <div className="landingPageBox">
-            <h1>Hello!</h1>
+    <div>
+        <header className="header">
+            <h3>About Me</h3>
+            <h3>Programming Experience</h3>
+            <h3>Fun Facts!</h3>
+        </header>
+        
+        <div className="landingPageContainer">
+            <div className="landingPageBox">
+                <h1>Hello!</h1>
+            </div>
         </div>
     </div>
     );
