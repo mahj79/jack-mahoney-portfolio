0866278b2ea3bccafcd4c558928c99b5e517c410
mahj79 <mahonej79@gmail.com>
2023-05-02 20:10:14 -0500

    added props to header for nav to carousel slides

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,21 +1,26 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import AboutMeBox from '../AboutMe/AboutMeBox';
-import Carousel from 'antd/es/carousel';
+import Carousel, { CarouselRef } from 'antd/es/carousel';
 import Aspirations from '../Aspirations/Aspirations';
 import FunFacts from '../FunFacts/FunFacts';
 import LandingPage from '../LandingPage/LandingPage';
 
-const App: React.FC = () => (
+
+const App: React.FC = () =>  {
   
+const slider = useRef<CarouselRef | null>()
+
+  
+return(  
   <div className="body" style={{overflow: 'hidden'}}>
-    <Carousel>
-      <LandingPage/>
+    <Carousel ref={slide => slider.current=slide}>
+      {slider.current && <LandingPage slider={slider.current}/>}
       <AboutMeBox/>
       <Aspirations/>
       <FunFacts/>
     </Carousel>
   </div>
 );
-
+}
 export default App;
 
--- src/Components/LandingPage/LandingPage.tsx
+++ src/Components/LandingPage/LandingPage.tsx
@@ -1,13 +1,20 @@
+import { CarouselRef } from 'antd/es/carousel';
 import React from 'react';
 
-export const LandingPage: React.FC = () => {
+export interface LandingPageProps {
+    slider: CarouselRef | null;
+}
+
+export const LandingPage: React.FC<LandingPageProps> = (props) => {
+
+const {slider} = props;
 
 return (
     <div>
         <header className="header">
-            <h4>About Me</h4>            
-            <h4>Programming </h4>
-            <h4>Fun Facts!</h4>
+            <h4 onClick={() => slider?.goTo(1)}>About Me</h4>            
+            <h4 onClick={() => slider?.goTo(2)}>Programming </h4>
+            <h4 onClick={() => slider?.goTo(3)}>Fun Facts!</h4>
         </header>
         
         <div className="landingPageContainer">
