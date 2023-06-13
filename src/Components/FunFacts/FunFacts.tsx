a6f703d946923726efb8c817d2c087050ad3ec81
mahj79 <mahonej79@gmail.com>
2023-06-12 21:41:59 -0500

    Revert "minor changes to Fun Facts for removal of unnecessary code and a slight punctuation miss"
    This reverts commit 2d667d2b6930ef46c95628ba3a4b103c440fdf0f.

--- src/Components/FunFacts/FunFacts.tsx
+++ src/Components/FunFacts/FunFacts.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { SpotifyService } from './Spotify/Util/SpotifyService';
 import { IPlaylist } from './Spotify/Data/IPlaylist';
+import slider from 'antd/es/slider';
 import { CarouselRef } from 'antd/es/carousel';
 
 export interface LandingPageProps {
@@ -53,7 +54,7 @@ export const FunFacts: React.FC<LandingPageProps> = (props) => {
                 <br></br>
                 <li>I'm an avid Detroit sports fan.</li>
                 <br></br>
-                <li>Procurer of many memes.</li>
+                <li>Procurer of many memes</li>
                 <br></br>
                 <li>Every single photo used on this website was taken by one of my friends!</li>
               </ul>
